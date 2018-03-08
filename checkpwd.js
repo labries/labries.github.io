4b441d83dd3f595d8942fdbc7a8994afc2b85c0a
labries <36988047+labries@users.noreply.github.com>
2018-03-08 12:05:41 +0800

    Create checkpwd.js

---
+++ checkpwd.js
@@ -0,0 +1,21 @@
+// 校验用户名及密码
+function checklogin(){
+	var  username=document.getElementById("username").value;
+	var  password=document.getElementById("password").value;
+	if(username==""||username==null ){
+		alert("Please enter name !");
+		return false;
+	}else if(username !== "a"){
+		alert("Wrong name !");
+		return false;
+	}else if(password==""||password==null){
+		alert("Please enter password!");
+		return false;
+	}else if (password !=="a"){
+		alert("Wrong password !");
+		return false;
+	}
+	return true;
+}
+
+
